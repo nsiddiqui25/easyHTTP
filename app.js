9427d2315d09d7e92cb710f0ff0ae6bb66e406f9
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2019-01-03 18:44:05 -0600

    easyHTTP - custom HTTP library - AJAX with callbacks

---
+++ app.js
@@ -0,0 +1,1 @@
+// this file is what uses our library
---
+++ easyhttp.js
@@ -0,0 +1,7 @@
+// this file is the actual library
+
+/*
+EasyHTTP is a custom AJAX Library
+   - In order to test, we'll need something to test with. We'll need a RESTful API that utilizes GET-POST-PUT DELETE requests
+*/
+
